8df00054911230bea18c71c935df4b0825d0cd98
jmanumaya <jm.maya@iesnervion.es>
2025-01-26 00:17:49 +0100

    No hecho, preguntar que hacer exactamente a Elena

---
+++ src/parte1/Ejer9.java
@@ -0,0 +1,19 @@
+package parte1;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Ejer9 {
+
+
+	public static void main(String[] args) {
+		// Colecciones para los números y estrellas
+		TreeMap<Integer, Integer> numeros = new TreeMap<>();
+		TreeMap<Integer, Integer> estrellas = new TreeMap<>();
+
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+	}
+
+}
